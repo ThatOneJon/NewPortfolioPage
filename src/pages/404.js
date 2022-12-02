e4e5d7cb28c7b4d89b86d47c83f5541bfa70d371
thatOneJon <83345498+ThatOneJon@users.noreply.github.com>
2022-12-02 23:04:09 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,16 @@
 import React from "react"
-
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 export default function Error(){
     return (
-        <h1>ERRRROR!</h1>
+        <Container>
+            <Row lg={1} className="justify-content-center align-items-center" style={{height:"100vh"}}>
+                <Col className="text-center">
+                    <h1 style={{fontSize:"90px"}}>404 - Page not Found!</h1>
+                </Col>
+            </Row>
+        </Container>
     )
 }
